71db93a2c28615e427db942cb4ea4210f3a272ef
Adam <adam.j.major@gmail.com>
2021-03-03 14:47:55 -0800

    Add geolocation stuff

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	window.navigator.geolocation.getCurrentPosition(
+		function (position) {
+			console.log("position =", position);
+		},
+		(err) => {
+			console.log("err =", err);
+		},
+	);
+
+	return <SeasonDisplay></SeasonDisplay>;
+};
 
 export default App;
---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeasonDisplay = (props) => {
+    return (
+        <div>Season Display Here</div>
+    );
+}
+
+export default SeasonDisplay
