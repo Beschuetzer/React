19415a5480371beec437050755c07908d50dc0ec
Adam <adam.j.major@gmail.com>
2021-03-03 16:41:30 -0800

    2nd Version of seasons

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
-import SeasonDisplay from "./SeasonDisplay";
+import React from 'react';
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		function (position) {
-			console.log("position =", position);
-		},
-		(err) => {
-			console.log("err =", err);
-		},
-	);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-	return <SeasonDisplay></SeasonDisplay>;
-};
+        this.state = {
+            lat: null,
+        }
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log('position =', position);
+                this.setState({lat: position.coords.latitude});
+            },
+            err => {
+                console.log('err =', err);
+            }
+        )
+    }
+    render() {
+        return (
+            <div>{this.state.lat}</div>
+        );
+    }
+}
 
 export default App;
--- seasons/src/SeasonDisplay.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SeasonDisplay = (props) => {
-    return (
-        <div>Season Display Here</div>
-    );
-}
-
-export default SeasonDisplay
