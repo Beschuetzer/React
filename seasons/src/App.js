0381bf07546fe6ba5520664019e2e2ed86e68c7d
Adam <adam.j.major@gmail.com>
2021-03-03 21:39:36 -0800

    Add animations

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -7,6 +7,24 @@ class App extends React.Component {
         lat: null,
         errMsg: '',
     }
+
+    getClientLocation() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),    
+            err => this.setState({errMsg: err.message})
+        )
+    }
+    componentDidMount(){
+        console.log('Rendered to Screen------------------------------------------------');
+        this.getClientLocation();
+    }
+    componentDidUpdate() {
+        console.log('component updated------------------------------------------------');
+    }
+    componentWillUnmount() {
+        //called when a component is removed from the screen;  good place for cleanup (especially for non-React code)
+        console.log('test------------------------------------------------'); 
+    }
     renderContent() {
         if (this.state.errMsg && !this.state.lat) {
             return (
@@ -30,23 +48,6 @@ class App extends React.Component {
                 {this.renderContent()}
             </div>
         );
-    }
-    componentDidMount(){
-        console.log('Rendered to Screen------------------------------------------------');
-        this.getClientLocation();
-    }
-    componentDidUpdate() {
-        console.log('component updated------------------------------------------------');
-    }
-    componentWillUnmount() {
-        //called when a component is removed from the screen;  good place for cleanup (especially for non-React code)
-        console.log('test------------------------------------------------'); 
-    }
-    getClientLocation() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({lat: position.coords.latitude}),    
-            err => this.setState({errMsg: err.message})
-        )
     }
 }
 
