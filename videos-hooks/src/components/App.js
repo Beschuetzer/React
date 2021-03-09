591cb7e4e8d7a98ad0b6d0d6a2d56c77cfc8bbfc
Adam <adam.j.major@gmail.com>
2021-03-08 17:07:47 -0800

    Refactor app to use function component

--- videos-hooks/src/components/App.js
+++ videos-hooks/src/components/App.js
@@ -3,54 +3,45 @@ import youtube from '../apis/youtube';
 import SearchBar from './SearchBar';
 import VideoDetail from './VideoDetail';
 import VideoList from './VideoList';
+import {useState, useEffect} from 'react';
 
 import './css/style.css';
-export default class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            videos: [],
-            selectedVideo: null,
-        }
+
+const App = () => {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const onVideoSelect = (video) => {
+        setSelectedVideo(video);
     }
-    onVideoSelect = (video) => {
-        console.log('video =', video);
-        this.setState({
-            selectedVideo: video,
-        });
-    }
-    onSearchSubmit = async (searchQuery) => {
+
+    const onSearchSubmit = async (searchQuery) => {
         try {
             const results = await youtube.get(`/search`, {
                 params: {
                     q: searchQuery,
                 }
             });
-            console.log('results =', results);
-            this.setState({
-                videos: results.data.items,
-                selectedVideo: results.data.items[0],
-            })
+            setVideos(results.data.items);
+            setSelectedVideo(results.data.items[0]);
         } catch (error) {
             console.log('error =', error);
         }
     }
-    componentDidMount = () => {
-        //called once when component finishes rendering initially
-        this.onSearchSubmit('my app');
-    }
-    componentDidUpdate = () => {
-        //called everytime  a component updates itself
-    }
-    render() {
-        return (
-            <div className="grid">
-                <SearchBar onSearchSubmit={this.onSearchSubmit}/>
-                <div className="video-grid">
-                    <VideoDetail selectedVideo={this.state.selectedVideo}/>
-                    <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
-                </div>
+
+    useEffect(() => {
+        onSearchSubmit('Welcome');
+    }, [])
+
+    return (
+        <div className="grid">
+            <SearchBar onSearchSubmit={onSearchSubmit}/>
+            <div className="video-grid">
+                <VideoDetail selectedVideo={selectedVideo}/>
+                <VideoList videos={videos} onVideoSelect={onVideoSelect}/>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default App;
