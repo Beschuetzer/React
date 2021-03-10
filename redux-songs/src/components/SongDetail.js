aaffcb79923a50451ef9ccc3aa44f957dbc1ee45
Adam <adam.j.major@gmail.com>
2021-03-09 19:01:00 -0800

    Finish

--- redux-songs/src/components/SongDetail.js
+++ redux-songs/src/components/SongDetail.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-const SongDetail = () => {
-    return (
-        <div>SongDetail here</div>
-    );
+const SongDetail = ({selectedSong}) => {
+    console.log('selectedSong =', selectedSong);
+    if (selectedSong) {
+        return (        
+            <div className="song-detail">
+                <h3>Now Playing:</h3>
+                <h4 className="song-detail__title">{selectedSong.title}</h4>
+                <h5 className="song-detail__artist">{selectedSong.artist}</h5>
+                <h4 className="song-detail__duration">{selectedSong.duration}</h4>
+            </div>
+        );
+    }
+    else {
+        return (        
+            <div className="song-detail song-detail--nullMsg">
+                Select a Song
+            </div>
+        );
+    }
 }
 
-export default SongDetail;
+const mapStateToProps = state => {
+    return {
+        selectedSong: state.selectedSong,
+    }
+}
+
+export default connect(mapStateToProps)(SongDetail);
