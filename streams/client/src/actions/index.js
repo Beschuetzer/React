a8c435accb2cdce81912cf404f8e8c82efacef99
Adam <adam.j.major@gmail.com>
2021-03-11 08:39:17 -0800

    Start streaming app;  setup routing

---
+++ streams/client/src/actions/index.js
@@ -0,0 +1,9 @@
+
+//Example Action Creator
+//export const selectSong = (song) => {
+//    return {
+//        type: "SONG_SELECTED",
+//        payload: song,
+//    }
+//}
+    
---
+++ streams/client/src/reducers/index.js
@@ -0,0 +1,15 @@
+
+import {combineReducers} from "redux";
+
+//Reducers accept an "Action Creator Function" as last arg
+//const selectedSongReducer = (selectedSong = null, action) => {
+//    if (action.type === "SONG_SELECTED") return action.payload;
+//    else return selectedSong;
+//}
+
+export default combineReducers({
+    //reducer names and desired keys go here
+    //e.g songs: selectedSongReducer,
+})
+
+    
