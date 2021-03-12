475ed7919568b90862807b801c0fc5a7005e370f
Adam <adam.j.major@gmail.com>
2021-03-11 17:28:36 -0800

    Setup redux-form inside StreamCreate

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,9 +1,11 @@
 
 import {combineReducers} from "redux";
 import authReducer from "./authReducer";
+import { reducer } from 'redux-form';
 
 export default combineReducers({
     auth: authReducer,
+    form: reducer,
 })
 
     
