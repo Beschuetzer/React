ab783d40edbaa7a563e7da72247974f313163c95
Adam <adam.j.major@gmail.com>
2021-03-13 17:56:04 -0800

    Add StreamDelete and StreamShow basic functionality

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-    return (
-        <div>StreamDelete</div>
-    );
+class StreamDelete extends React.Component {
+    componentDidMount() {
+        console.log('this.props.stream =', this.props.stream);
+        if (!this.props.stream) this.props.fetchStream(this.props.match.params.id);
+    }
+    render() {
+        if (!this.props.stream) return <div>Loading</div>
+        return (
+            <div>
+                Deleting...
+               <h4>{this.props.stream.title}</h4> 
+            </div>
+        );
+    }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+    console.log('ownProps =', ownProps);
+    return {
+        stream: state.streams[ownProps.match.params.id],
+    }
+}
+
+export default connect(mapStateToProps, {
+    fetchStream,
+})(StreamDelete);
--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-	return <div>StreamShow</div>;
+class StreamShow extends React.Component {
+	componentDidMount() {
+		if (!this.props.stream) this.props.fetchStream(this.props.match.params.id);
+	}
+	render() {
+		if (!this.props.stream) return (
+			<div>
+				Loading Stream...
+			</div>
+		);
+		
+		return (
+				<div>
+					Showing:
+					<h3>{this.props.stream.title}</h3>
+				</div>
+			);
+	}
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+	return {
+		stream: state.streams[ownProps.match.params.id],
+	}
+}
+
+export default connect(mapStateToProps, {
+	fetchStream,
+})(StreamShow);
