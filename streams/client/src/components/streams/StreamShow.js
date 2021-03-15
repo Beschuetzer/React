165212f5c799886f4931e7b74acb951eeb6e046d
Adam <adam.j.major@gmail.com>
2021-03-14 20:18:26 -0700

    finish rtmp server

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,11 +1,42 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
 class StreamShow extends React.Component {
-	componentDidMount() {
-		if (!this.props.stream) this.props.fetchStream(this.props.match.params.id);
+	constructor(props) {
+		super(props);
+		this.videoRef = React.createRef();
 	}
+	componentDidMount() {
+		const { id } = this.props.match.params;
+
+		if (!this.props.stream) {
+			this.props.fetchStream(id);
+		}
+		this.buildPlayer();		
+	}
+
+	componentDidUpdate() {
+		this.buildPlayer();
+	}
+
+	componentWillUnmount() {
+		//clean up the stream so that the feed is removed when navigating away
+		this.flvPlayer.destroy();
+	}
+	buildPlayer() {
+		if (this.flvPlayer || !this.props.stream) return;
+		const { id } = this.props.match.params;
+		this.flvPlayer = flv.createPlayer({
+			type: 'flv',
+			url: `http://localhost:8000/live/${id}.flv`
+		});
+		console.log('this.flvPlayer =', this.flvPlayer);
+		this.flvPlayer.attachMediaElement(this.videoRef.current);
+		this.flvPlayer.load();
+	}
+
 	render() {
 		if (!this.props.stream) return (
 			<div>
@@ -15,6 +46,7 @@ class StreamShow extends React.Component {
 		const {title, description} = this.props.stream;
 		return (
 				<div>
+					<video ref={this.videoRef} style={{width: "100%"}} controls/>
 					<h1>{title}</h1>
 					<h5>{description}</h5>
 				</div>
